798f93423382b869d3cd649336653714cf927ebc
Blagovest <btatanasov@gmail.com>
2020-02-13 20:00:04 +0200

    Add files via upload

---
+++ Web_development_final_project/js/choose.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").onclick = function () {
+	location.href = "normal_solving.html";
+};
+
+document.getElementById("button2").onclick = function () {
+	location.href = "oh_solving.html";
+};
+
+document.getElementById("button3").onclick = function () {
+	location.href = "blind_solving.html";
+};
---
+++ Web_development_final_project/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("button").onclick = function () {
+	location.href = "choose.html";
+};
