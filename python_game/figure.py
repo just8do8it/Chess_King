be84e2b6baaeb79e454c8b35763be2be239e1eed
just8do8it <blagovest.t.atanasov.2016@elsys-bg.org>
2021-02-17 10:07:33 +0200

    figure.py segmentation

--- python_game/figure.py
+++ python_game/figure.py
@@ -47,116 +47,199 @@ class Figure:
 			self.name == "P3" or self.name == "P4" or \
 			self.name == "P5" or self.name == "P6" or \
 			self.name == "P7" or self.name == "P8":
-
-				if self.player == "white" and new_ltr == self.curr_pos_ltr and self.curr_pos_num == self.start_pos_num and new_pos_num == self.curr_pos_num + 2 or \
-					self.player == "white" and new_ltr == self.curr_pos_ltr and new_pos_num == self.curr_pos_num + 1 or \
-					self.player == "white" and new_ltr == self.curr_pos_ltr + 1 and new_pos_num == self.curr_pos_num + 1 or \
-					self.player == "white" and new_ltr == self.curr_pos_ltr - 1 and new_pos_num == self.curr_pos_num + 1 or \
-					self.player == "black" and self.curr_pos_num == self.start_pos_num and new_pos_num == self.curr_pos_num - 2 or \
-					self.player == "black" and new_ltr == self.curr_pos_ltr and new_pos_num == self.curr_pos_num - 1 or \
-					self.player == "black" and new_ltr == self.curr_pos_ltr + 1 and new_pos_num == self.curr_pos_num - 1 or \
-					self.player == "black" and new_ltr == self.curr_pos_ltr - 1 and new_pos_num == self.curr_pos_num - 1:
-					
-						if new_ltr == self.curr_pos_ltr + 1 or new_ltr == self.curr_pos_ltr - 1:
-							if self.board[new_pos_num - 1][new_pos_ltr] != None:
-								if test == 0:
-									self.curr_pos_num = new_pos_num
-									self.curr_pos_ltr = new_ltr
-									return True, new_pos_num, new_ltr
-								else:
-									return True
-							else:
-								# print("lost")
-								return False
-						else:
-							if self.board[new_pos_num - 1][new_pos_ltr] == None:
-								if test == 0:
-									self.curr_pos_num = new_pos_num
-									self.curr_pos_ltr = new_ltr
-								return True
-							else:
-								return False
-
-				else:
-					return False
+				return self.move_pawn(new_pos_num, new_pos_ltr, test, new_ltr)
 
 		elif self.name == "R1" or self.name == "R2":
+			return self.move_rook(new_pos_num, new_pos_ltr, test, new_ltr)
 
-			if new_pos_num > self.curr_pos_num:
+		elif self.name == "H1" or self.name == "H2":
+			return self.move_horse(new_pos_num, new_pos_ltr, test, new_ltr)
+
+		elif self.name == "B1" or self.name == "B2":
+			return self.move_bishop(new_pos_num, new_pos_ltr, test, new_ltr)
+
+		elif self.name == "Q1":
+			return self.move_queen(new_pos_num, new_pos_ltr, test, new_ltr)
+
+		elif self.name == "K1":
+			return self.move_king(new_pos_num, new_pos_ltr, test, new_ltr)
+
+
+	def move_pawn(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if self.player == "white" and new_ltr == self.curr_pos_ltr and self.curr_pos_num == self.start_pos_num and new_pos_num == self.curr_pos_num + 2 or \
+			self.player == "white" and new_ltr == self.curr_pos_ltr and new_pos_num == self.curr_pos_num + 1 or \
+			self.player == "white" and new_ltr == self.curr_pos_ltr + 1 and new_pos_num == self.curr_pos_num + 1 or \
+			self.player == "white" and new_ltr == self.curr_pos_ltr - 1 and new_pos_num == self.curr_pos_num + 1 or \
+			self.player == "black" and self.curr_pos_num == self.start_pos_num and new_pos_num == self.curr_pos_num - 2 or \
+			self.player == "black" and new_ltr == self.curr_pos_ltr and new_pos_num == self.curr_pos_num - 1 or \
+			self.player == "black" and new_ltr == self.curr_pos_ltr + 1 and new_pos_num == self.curr_pos_num - 1 or \
+			self.player == "black" and new_ltr == self.curr_pos_ltr - 1 and new_pos_num == self.curr_pos_num - 1:
+			
+				if new_ltr == self.curr_pos_ltr + 1 or new_ltr == self.curr_pos_ltr - 1:
+					if self.board[new_pos_num - 1][new_pos_ltr] != None:
+						if test == 0:
+							self.curr_pos_num = new_pos_num
+							self.curr_pos_ltr = new_ltr
+							return True, new_pos_num, new_ltr
+						else:
+							return True
+					else:
+						return False
+				else:
+					if self.board[new_pos_num - 1][new_pos_ltr] == None:
+						if test == 0:
+							self.curr_pos_num = new_pos_num
+							self.curr_pos_ltr = new_ltr
+						return True
+					else:
+						return False
+
+		else:
+			return False
+	
+
+	def move_rook(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if new_pos_num > self.curr_pos_num:
 				if self.curr_pos_ltr != new_ltr:
 					return False
 				for num in range(self.curr_pos_num, new_pos_num - 1):
 					if self.board[num][chr(self.curr_pos_ltr)] != None:
 						return False	
 
-			elif new_pos_num < self.curr_pos_num:
-				if self.curr_pos_ltr != new_ltr:
+		elif new_pos_num < self.curr_pos_num:
+			if self.curr_pos_ltr != new_ltr:
+				return False
+			for num in range(new_pos_num, self.curr_pos_num - 1):
+				if self.board[num][chr(self.curr_pos_ltr)] != None:
 					return False
-				for num in range(new_pos_num, self.curr_pos_num - 1):
-					if self.board[num][chr(self.curr_pos_ltr)] != None:
-						return False
 
-			elif new_ltr > self.curr_pos_ltr:
-				if self.curr_pos_num != new_pos_num:
+		elif new_ltr > self.curr_pos_ltr:
+			if self.curr_pos_num != new_pos_num:
+				return False
+			for ltr in range(self.curr_pos_ltr + 1, new_ltr):
+				if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
 					return False
+
+		elif new_ltr < self.curr_pos_ltr:
+			if self.curr_pos_num != new_pos_num:
+				return False
+			for ltr in range(new_ltr + 1, self.curr_pos_ltr):
+				if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
+					return False
+
+
+		if new_pos_num != self.curr_pos_num:
+			if test == 0:
+				self.curr_pos_num = new_pos_num
+			else:
+				return True
+
+			if self.board[new_pos_num - 1][new_pos_ltr] != None:
+				return True, new_pos_num, new_ltr
+			else:
+				return True
+
+		elif new_ltr != self.curr_pos_ltr:
+			if test == 0:
+				self.curr_pos_ltr = new_ltr
+			else:
+				return True
+
+			if self.board[new_pos_num - 1][new_pos_ltr] != None:
+				return True, new_pos_num, new_ltr
+			else:
+				return True
+		
+		else:
+			return False
+	
+
+	def move_horse(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if new_ltr < self.curr_pos_ltr - 2 or new_ltr > self.curr_pos_ltr + 2 or new_ltr == self.curr_pos_ltr or \
+				new_pos_num < self.curr_pos_num - 2 or new_pos_num > self.curr_pos_num + 2 or new_pos_num == self.curr_pos_num:
+					return False
+		else:
+			if abs(new_ltr - self.curr_pos_ltr) == 1 and abs(new_pos_num - self.curr_pos_num) == 1:
+				return False
+			else:
+				if test == 0:
+					self.curr_pos_num = new_pos_num
+					self.curr_pos_ltr = new_ltr
+				else:
+					return True
+				
+				if self.board[new_pos_num - 1][new_pos_ltr] != None:
+					return True, new_pos_num, new_ltr
+				else:
+					return True
+	
+	def move_bishop(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if abs(new_ltr - self.curr_pos_ltr) != abs(new_pos_num - self.curr_pos_num):
+				return False
+		else:
+			num_counter = 0
+			ltr_counter = 0
+			spec_num_counter = 0
+			spec_ltr_counter = 0
+
+			if new_ltr > self.curr_pos_ltr and new_pos_num > self.curr_pos_num:
+				spec_num_counter = 1
+				spec_ltr_counter = 1
+			elif new_ltr > self.curr_pos_ltr and new_pos_num < self.curr_pos_num:
+				spec_num_counter = -1
+				spec_ltr_counter = 1
+			elif new_ltr < self.curr_pos_ltr and new_pos_num < self.curr_pos_num:
+				spec_num_counter = -1
+				spec_ltr_counter = -1
+			elif new_ltr < self.curr_pos_ltr and new_pos_num > self.curr_pos_num:
+				spec_num_counter = 1
+				spec_ltr_counter = -1
+
+			num_counter += spec_num_counter
+			ltr_counter += spec_ltr_counter
+
+			for i in range(abs(new_ltr - self.curr_pos_ltr) - 1):
+				if self.board[self.curr_pos_num + num_counter - 1][chr(self.curr_pos_ltr + ltr_counter)] != None:
+					return False
+				num_counter += spec_num_counter
+				ltr_counter += spec_ltr_counter
+			
+			if test == 0:
+				self.curr_pos_num = new_pos_num
+				self.curr_pos_ltr = new_ltr
+			else:
+				return True
+			
+			if self.board[new_pos_num - 1][new_pos_ltr] != None:
+				return True, new_pos_num, new_ltr
+			else:
+				return True
+
+
+	def move_queen(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if new_ltr == self.curr_pos_ltr:
+				if new_pos_num > self.curr_pos_num:
+					for num in range(self.curr_pos_num, new_pos_num - 1):
+						if self.board[num][chr(self.curr_pos_ltr)] != None:
+							return False	
+
+				elif new_pos_num < self.curr_pos_num:
+					for num in range(new_pos_num, self.curr_pos_num - 1):
+						if self.board[num][chr(self.curr_pos_ltr)] != None:
+							return False
+				
+		elif new_pos_num == self.curr_pos_num:
+			if new_ltr > self.curr_pos_ltr:
 				for ltr in range(self.curr_pos_ltr + 1, new_ltr):
 					if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
 						return False
 
 			elif new_ltr < self.curr_pos_ltr:
-				if self.curr_pos_num != new_pos_num:
-					return False
 				for ltr in range(new_ltr + 1, self.curr_pos_ltr):
 					if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
 						return False
 
-
-			if new_pos_num != self.curr_pos_num:
-				if test == 0:
-					self.curr_pos_num = new_pos_num
-				else:
-					return True
-
-				if self.board[new_pos_num - 1][new_pos_ltr] != None:
-					return True, new_pos_num, new_ltr
-				else:
-					return True
-
-			elif new_ltr != self.curr_pos_ltr:
-				if test == 0:
-					self.curr_pos_ltr = new_ltr
-				else:
-					return True
-
-				if self.board[new_pos_num - 1][new_pos_ltr] != None:
-					return True, new_pos_num, new_ltr
-				else:
-					return True
-			
-			else:
-				return False
-
-
-		elif self.name == "H1" or self.name == "H2":
-			if new_ltr < self.curr_pos_ltr - 2 or new_ltr > self.curr_pos_ltr + 2 or new_ltr == self.curr_pos_ltr or \
-				new_pos_num < self.curr_pos_num - 2 or new_pos_num > self.curr_pos_num + 2 or new_pos_num == self.curr_pos_num:
-					return False
-			else:
-				if abs(new_ltr - self.curr_pos_ltr) == 1 and abs(new_pos_num - self.curr_pos_num) == 1:
-					return False
-				else:
-					if test == 0:
-						self.curr_pos_num = new_pos_num
-						self.curr_pos_ltr = new_ltr
-					else:
-						return True
-					
-					if self.board[new_pos_num - 1][new_pos_ltr] != None:
-						return True, new_pos_num, new_ltr
-					else:
-						return True
-
-		elif self.name == "B1" or self.name == "B2":
+		else:
 			if abs(new_ltr - self.curr_pos_ltr) != abs(new_pos_num - self.curr_pos_num):
 				return False
 			else:
@@ -186,96 +269,30 @@ class Figure:
 						return False
 					num_counter += spec_num_counter
 					ltr_counter += spec_ltr_counter
-				
-				if test == 0:
-					self.curr_pos_num = new_pos_num
-					self.curr_pos_ltr = new_ltr
-				else:
-					return True
-				
-				if self.board[new_pos_num - 1][new_pos_ltr] != None:
-					return True, new_pos_num, new_ltr
-				else:
-					return True
 
+		if test == 0:
+			self.curr_pos_num = new_pos_num
+			self.curr_pos_ltr = new_ltr
+		else:
+			return True
+		
+		if self.board[new_pos_num - 1][new_pos_ltr] != None:
+			return True, new_pos_num, new_ltr
+		else:
+			return True
 
-		elif self.name == "Q1":
-			if new_ltr == self.curr_pos_ltr:
-				if new_pos_num > self.curr_pos_num:
-					for num in range(self.curr_pos_num, new_pos_num - 1):
-						if self.board[num][chr(self.curr_pos_ltr)] != None:
-							return False	
-
-				elif new_pos_num < self.curr_pos_num:
-					for num in range(new_pos_num, self.curr_pos_num - 1):
-						if self.board[num][chr(self.curr_pos_ltr)] != None:
-							return False
-				
-			elif new_pos_num == self.curr_pos_num:
-				if new_ltr > self.curr_pos_ltr:
-					for ltr in range(self.curr_pos_ltr + 1, new_ltr):
-						if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
-							return False
-
-				elif new_ltr < self.curr_pos_ltr:
-					for ltr in range(new_ltr + 1, self.curr_pos_ltr):
-						if self.board[self.curr_pos_num - 1][chr(ltr)] != None:
-							return False
-
-			else:
-				if abs(new_ltr - self.curr_pos_ltr) != abs(new_pos_num - self.curr_pos_num):
-					return False
-				else:
-					num_counter = 0
-					ltr_counter = 0
-					spec_num_counter = 0
-					spec_ltr_counter = 0
-
-					if new_ltr > self.curr_pos_ltr and new_pos_num > self.curr_pos_num:
-						spec_num_counter = 1
-						spec_ltr_counter = 1
-					elif new_ltr > self.curr_pos_ltr and new_pos_num < self.curr_pos_num:
-						spec_num_counter = -1
-						spec_ltr_counter = 1
-					elif new_ltr < self.curr_pos_ltr and new_pos_num < self.curr_pos_num:
-						spec_num_counter = -1
-						spec_ltr_counter = -1
-					elif new_ltr < self.curr_pos_ltr and new_pos_num > self.curr_pos_num:
-						spec_num_counter = 1
-						spec_ltr_counter = -1
-
-					num_counter += spec_num_counter
-					ltr_counter += spec_ltr_counter
-
-					for i in range(abs(new_ltr - self.curr_pos_ltr) - 1):
-						if self.board[self.curr_pos_num + num_counter - 1][chr(self.curr_pos_ltr + ltr_counter)] != None:
-							return False
-						num_counter += spec_num_counter
-						ltr_counter += spec_ltr_counter
-
-			
-			if test == 0:
-				self.curr_pos_num = new_pos_num
-				self.curr_pos_ltr = new_ltr
-			else:
-				return True
-			
-			if self.board[new_pos_num - 1][new_pos_ltr] != None:
-				return True, new_pos_num, new_ltr
-			else:
-				return True
-
-		elif self.name == "K1":
-			if abs(new_pos_num - self.curr_pos_num) > 1 or abs(new_ltr - self.curr_pos_ltr) > 1:
+	
+	def move_king(self, new_pos_num, new_pos_ltr, test, new_ltr):
+		if abs(new_pos_num - self.curr_pos_num) > 1 or abs(new_ltr - self.curr_pos_ltr) > 1:
 				return False
+		
+		if test == 0:
+			self.curr_pos_num = new_pos_num
+			self.curr_pos_ltr = new_ltr
+		else:
+			return True
 
-			if test == 0:
-				self.curr_pos_num = new_pos_num
-				self.curr_pos_ltr = new_ltr
-			else:
-				return True
-
-			if self.board[new_pos_num - 1][new_pos_ltr] != None:
-				return True, new_pos_num, new_ltr
-			else:
-				return True
+		if self.board[new_pos_num - 1][new_pos_ltr] != None:
+			return True, new_pos_num, new_ltr
+		else:
+			return True
